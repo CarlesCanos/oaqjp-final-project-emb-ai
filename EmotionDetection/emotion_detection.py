0c36ace1900a34f51d9795832ae7939cdb51386b
Carles <carlescanos@gmail.com>
2025-07-15 12:34:45 -0400

    final commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,32 +6,45 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = body, headers=headers)
-    formatted_response = json.loads(response.text)
+    
+    print(response)
 
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-
-    emotions = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
-    emotionLabels = ['anger', 'disgust', 'fear', 'joy', 'sadness']
-
-    currentDominant = 0
-    currentDominantIndex = 0
-
-    for index, emotion in  enumerate(emotions):
-        if(emotion > currentDominant):
-            currentDominant = emotion
-            currentDominantIndex = index
-            
-    dominant_emotion = emotionLabels[currentDominantIndex]
-
-    return {
-            'anger': anger_score,
-            'disgust': disgust_score,
-            'fear': fear_score,
-            'joy': joy_score,
-            'sadness': sadness_score,
-            'dominant_emotion': dominant_emotion
+    if "400" in str(response):
+        return {
+            'anger': 'None',
+            'disgust': 'None',
+            'fear': 'None',
+            'joy': 'None',
+            'sadness': 'None',
+            'dominant_emotion': 'None'
             }
+    else:
+        formatted_response = json.loads(response.text)
+
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+        emotions = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+        emotionLabels = ['anger', 'disgust', 'fear', 'joy', 'sadness']
+
+        currentDominant = 0
+        currentDominantIndex = 0
+
+        for index, emotion in  enumerate(emotions):
+            if emotion > currentDominant:
+                currentDominant = emotion
+                currentDominantIndex = index
+                
+        dominant_emotion = emotionLabels[currentDominantIndex]
+
+        return {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': dominant_emotion
+                }
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Server to check text emotions"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +6,18 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Function returning index page"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def sent_analyzer():
+def emotion_detector_endpoint():
+    """Function returning analysis from text"""
     text_to_detect = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_detect)
+
+    if response['dominant_emotion'] == 'None':
+        return 'Invalid text! Please try again!'
+
 
     return  (
             f"For the given statement, the system response is "
